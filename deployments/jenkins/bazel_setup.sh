2a52ac9607f6f8cc5e72c18555d315a93b60f324
kubeuser <kubeuser@gmail.com>
2021-04-01 21:29:19 -0400

    Jenkins test: add script ip

--- deployments/jenkins/bazel_setup.sh
+++ deployments/jenkins/bazel_setup.sh
@@ -5,3 +5,6 @@ touch ~/.kube/config
 cp deployments/jenkins/bazel.kubeconfig ~/.kube/config
 cat /var/run/secrets/kubernetes.io/serviceaccount/token | xargs -I @ sed -i "s/\${TOKEN}/@/" ~/.kube/config
 echo $KUBERNETES_SERVICE_HOST | xargs -I @ sed -i "s/\${KUBERNETES_SERVICE_HOST}/@/" ~/.kube/config
+
+cluster_ip=`kubectl get svc/registry-docker-registry -o yaml  | grep clusterIP | awk '{print $2}'`
+echo $cluster_ip | xargs -I @ sed -i "s/localhost:5000/@:5000/" WORKSPACE
