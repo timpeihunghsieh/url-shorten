84633cd4a1ca91a0b0bb192cbf94054fd2c7577b
kubeuser <kubeuser@gmail.com>
2021-04-02 00:01:58 -0400

    Added start up script and ingress files for elk and prometheus

---
+++ startup/start-cluster.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Prometheus stack
+helm install prometheus prometheus-community/kube-prometheus-stack --version "14.0.0"
+kubectl apply -f deployments/prometheus-stack/prometheus-ingress.yaml
+kubectl apply -f deployments/prometheus-stack/alertmanager-ingress.yaml
+kubectl apply -f deployments/prometheus-stack/grafana-ingress.yaml
+
+# ELK
+kubectl apply -f https://download.elastic.co/downloads/eck/1.5.0/all-in-one.yaml
+sleep 5  # TODO(timhsieh): hack to wait for CRD to be deployed. 
+kubectl apply -f deployments/elk-stack/filebeat_autodiscover.yaml
+kubectl apply -f deployments/elk-stack/kibana-ingress.yaml
---
+++ startup/start-minikube.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+if  ! which kubectl &>/dev/null ; then
+    echo "Please install kubectl (>v1.18.0)"
+    exit 1
+fi
+
+if  ! which minikube &>/dev/null ; then
+    echo "Please install minikube (>v1.18.1)"
+    exit 1
+fi
+
+if  ! which helm &>/dev/null ; then
+    echo "Please install helm (>3.0.0)"
+    exit 1
+fi
+
+echo "Starting Minikube..."
+minikube start \
+    --kubernetes-version=v1.18.0 \
+    --vm-driver=virtualbox \
+    --cpus=4 \
+    --memory=4096 \
+    --disk-size=10g
+
+minikube_ip=$(minikube ip)
+if [[ -z "${minikube_ip}" ]]; then
+    echo "Cannot find minikube. Exiting.."
+    exit 1
+fi
+
+echo "Starting minikube's ingress addon."
+if ! minikube addons enable ingress ; then
+    echo "Failed to start minikube's ingress addon."
+    exit 1
+fi
+
+sudo -- sh -c "cp /etc/hosts /tmp/hosts.backup"
+sudo -- sh -c "cat /tmp/hosts.backup | grep -v tim.hsieh > /etc/hosts"
+sudo -- sh -c "echo '# Bazel entries for tim.hsieh' >> /etc/hosts"
+sudo -- sh -c "echo \"${minikube_ip}     prometheus.tim.hsieh\" >> /etc/hosts"
+sudo -- sh -c "echo \"${minikube_ip}     grafana.tim.hsieh\" >> /etc/hosts"
+sudo -- sh -c "echo \"${minikube_ip}     alertmanager.tim.hsieh\" >> /etc/hosts"
+sudo -- sh -c "echo \"${minikube_ip}     kibana.tim.hsieh\" >> /etc/hosts"
+
+echo "Finished Setting up!"
