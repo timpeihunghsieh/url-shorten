0a4db8f2ac2d9e5b86602f05524c66a4066fcc66
Tim Hsieh <timpeihunghsieh@example.com>
2021-04-17 16:24:30 -0400

    Remove shorten URL canary

--- startup/minikube/start-cluster.sh
+++ startup/minikube/start-cluster.sh
@@ -5,9 +5,16 @@
 # TODO(timhsieh): configure this so that they can be configured together.
 
 # Prometheus stack
-helm install prometheus prometheus-community/kube-prometheus-stack --version "14.0.0"
+# helm install prometheus prometheus-community/kube-prometheus-stack --version "14.0.0"
 
 # ELK
-kubectl apply -f https://download.elastic.co/downloads/eck/1.5.0/all-in-one.yaml
-sleep 5  # TODO(timhsieh): hack to wait for CRD to be deployed. 
-kubectl apply -f deployments/elk-stack/filebeat_autodiscover.yaml
+#kubectl apply -f https://download.elastic.co/downloads/eck/1.5.0/all-in-one.yaml
+#sleep 5  # TODO(timhsieh): hack to wait for CRD to be deployed. 
+#kubectl apply -f deployments/elk-stack/filebeat_autodiscover.yaml
+
+# URL shorten
+bazel run deployments/url-shorten/minikube:k8-all.apply
+
+# Ingress rules
+kubectl apply -f deployments/ingress/internal-services-ingress.yaml
+kubectl apply -f deployments/ingress/external-services-ingress.yaml
