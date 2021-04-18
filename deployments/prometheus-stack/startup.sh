a3d069c9d578cfb9a4f4ccfd75d26e018dc3e772
Tim Hsieh <timpeihunghsieh@example.com>
2021-04-17 22:18:35 -0400

    Added Prometheus to monitor URL shorten in default namespace

--- deployments/prometheus-stack/startup.sh
+++ deployments/prometheus-stack/startup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-kubectl namespace create metrics-monitoring
+#kubectl label namespace default metrics-monitoring=target
+#kubectl namespace create metrics-monitoring -l metrics-monitoring=target
 helm install prometheus prometheus-community/kube-prometheus-stack \
   --version "14.0.0" \
-  -f values.yaml
+  -f deployments/prometheus-stack/values.yaml
