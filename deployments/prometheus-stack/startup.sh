0d6969c84a3e8dfc3fc4790559f802eb66a294d8
Tim Hsieh <timpeihunghsieh@example.com>
2021-04-17 22:32:00 -0400

    Fix the prometheus startup script

--- deployments/prometheus-stack/startup.sh
+++ deployments/prometheus-stack/startup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-#kubectl label namespace default metrics-monitoring=target
-#kubectl namespace create metrics-monitoring -l metrics-monitoring=target
+kubectl label namespace default metrics-monitoring=target
+kubectl create namespace metrics-monitoring
+kubectl label namespace metrics-monitoring metrics-monitoring=target
 helm install prometheus prometheus-community/kube-prometheus-stack \
   --version "14.0.0" \
   -f deployments/prometheus-stack/values.yaml
